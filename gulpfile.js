44c23161cb8f5c01f83f2c8675720f7c28f015e7
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-12 00:45:40 +0500

    npm init

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,39 @@
 'use strict';
 
 global.plugins = {
-  gulp:         require('gulp'),
-  plumber:      require('gulp-plumber'),
-  sass:         require('gulp-plumber'),
-  postcss:      require('gulp-postcss'),
-  autoprefixer: require('autoprefixer'),
-  rename:       require('gulp-rename'),
-  minify:       require('gulp-csso'),
-  concat:       require('gulp-concat'),
-  uglify:       require("gulp-uglifyjs"),
-  imagemin:     require("gulp-imagemin"),
-  webp:         require("gulp-webp"),
-  del:          require("del"),
-  server:       require('browser-sync').create(),
+    gulp: require('gulp'),
+    plumber: require('gulp-plumber'),
+    sass: require('gulp-plumber'),
+    postcss: require('gulp-postcss'),
+    autoprefixer: require('autoprefixer'),
+    rename: require('gulp-rename'),
+    minify: require('gulp-csso'),
+    concat: require('gulp-concat'),
+    uglify: require("gulp-uglifyjs"),
+    imagemin: require("gulp-imagemin"),
+    webp: require("gulp-webp"),
+    del: require("del"),
+    server: require('browser-sync').create(),
 
-  path: {
-    tasks: require('./gulp/config/tasks.js')
-  }
+    path: {
+        tasks: require('./gulp/config/tasks.js')
+    }
 };
 
 let modulesArray = plugins.path.tasks;
 
-modulesArray.forEach(function (taskPath) {
-  require(taskPath)();
+modulesArray.forEach(function(taskPath) {
+    require(taskPath)();
 });
 
 plugins.gulp.task("default", plugins.gulp.series(
-  plugins.gulp.parallel('style', 'scripts', 'images:dev'),
-  plugins.gulp.parallel('watch', 'serve')
+    plugins.gulp.parallel('style', 'scripts', 'images:dev'),
+    plugins.gulp.parallel('watch', 'serve')
 ));
 
 plugins.gulp.task("build", plugins.gulp.series(
-  plugins.gulp.parallel('delete'),
-  plugins.gulp.parallel('style', 'scripts'),
-  plugins.gulp.parallel('images:build', 'webp'),
-  plugins.gulp.parallel('watch', 'serve')
+    plugins.gulp.parallel('delete'),
+    plugins.gulp.parallel('style', 'scripts'),
+    plugins.gulp.parallel('images:build', 'webp'),
+    plugins.gulp.parallel('watch', 'serve')
 ));
